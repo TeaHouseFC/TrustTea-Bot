be55b982e9a43e76cf6123aeb7e1f95a795415c4
Peekaey <dallasjt31@gmail.com>
2024-08-20 20:51:54 +1000

    Fix Bugs, Add Tests

---
+++ src/Tests/LodestoneHelpersTest.test.ts
@@ -0,0 +1,27 @@
+import {expect, test} from "bun:test"
+import * as LodestoneHelpers from "../Helpers/LodestoneHelpers"
+// Test Parameters To Expect Returned FC/Character Id
+const ReturnedFirstName = "Art"
+const ReturnedLastName = "Bayard"
+const server = "Carbuncle"
+
+console.log("Starting Tests for LodestoneHelpers.....")
+
+
+test('Call LodestoneHelpers GetLodestoneCharacterId', async() => {
+    let characterId = await LodestoneHelpers.GetLodestoneCharacterId(ReturnedFirstName, ReturnedLastName)
+    expect(characterId).toBe("36234146");
+});
+
+test('Call LodestoneHelpers GetLodestoneFreeCompany', async() => {
+    let freeCompany = await LodestoneHelpers.GetLodestoneFreeCompany(ReturnedFirstName, ReturnedLastName)
+    expect(freeCompany).toBe("The Tea House");
+});
+
+test('Call LodestoneHelpers RobustlyCheckLodestoneFreeCompany', async() => {
+    let freeCompany = await LodestoneHelpers.RobustlyCheckLodestoneFreeCompany(ReturnedFirstName, ReturnedLastName)
+    expect(freeCompany).toBe("The Tea House");
+});
+
+
+
